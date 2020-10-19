16ee61fa22894d50be091cbbf2a9211c94385ab6
Ines <inz@e-nno.ch>
2020-10-19 18:04:44 +0200

    [ADD] swagger documentation

--- app/router/router.js
+++ app/router/router.js
@@ -1,42 +1,22 @@
 const verifySignUp = require("./verifySignUp");
 const authJwt = require("./verifyJwtToken");
 const swaggerUi = require("swagger-ui-express");
-const swaggerJsondoc = require("swagger-jsdoc");
+const swaggerDocument = require("../docs/swagger.json");
 
-const options = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "E-nno API",
-      version: "1.0.0",
-      description: "E-nno intern API",
-      contact: {
-        name: "Ines Zenk",
-        url: "https://e-nno.ch",
-        email: "inz@e-nno.ch"
-      }
-    },
-    servers: [
-      {
-        url: "http://localhost:3000/"
-      }
-    ]
-  },
-  apis: []
+const swaggerOptions = {
+  swaggerOptions: {
+    validatorUrl: null
+  }
 };
 
 module.exports = function(app) {
   const controller = require("../controller/controller.js");
 
-  // Create swagger
-  const specs = swaggerJsondoc(options);
-  app.use("/docs", swaggerUi.serve);
-
-  app.get(
-    "/docs",
-    swaggerUi.setup(specs, {
-      explorer: true
-    })
+  // Swagger documentation
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, swaggerOptions)
   );
 
   app.post(
--- server.js
+++ server.js
@@ -24,20 +24,3 @@ var server = app.listen(8080, function() {
 
   console.log("App listening at http://%s:%s", host, port);
 });
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "USER"
-  });
-
-  Role.create({
-    id: 2,
-    name: "ADMIN"
-  });
-
-  Role.create({
-    id: 3,
-    name: "PM"
-  });
-}
