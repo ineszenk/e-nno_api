de6c40678f4c4cd18a1e27c400f09673bf737b30
Ines <inz@e-nno.ch>
2021-02-24 15:22:20 +0100

    [ADD] lat long

---
+++ app/model/kwh_conso.model.js
@@ -0,0 +1,25 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, Sequelize) {
+  const Kwh_conso = sequelize.define("kwh_conso", {
+    id: {
+      autoIncrement: true,
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    tmp: {
+      type: Sequelize.DATE,
+      allowNull: true
+    },
+    value: {
+      type: Sequelize.DOUBLE,
+      allowNull: true
+    },
+    enno_serial: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    }
+  });
+  return Kwh_conso;
+};
