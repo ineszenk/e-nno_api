01bf80dbbe170b977a60240dc27ec457d7ba1565
Ines <inz@e-nno.ch>
2021-02-26 11:22:59 +0100

    [ADD] logs

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const { sequelize_connection } = require("./app/config/db.config");
 // Create a Server
 const PORT = process.env.PORT || 8000;
 
-sequelize_connection().then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+sequelize_connection()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  .catch(error => {
+    console.error(error);
   });
-});
