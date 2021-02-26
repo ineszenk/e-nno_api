d165e52a12240f7ca6ddedd3f7242e5a15e8d212
Ines <inz@e-nno.ch>
2021-02-26 13:26:10 +0100

    [UPDATE] graphics endpoint

--- app/controller/graphicsController.js
+++ app/controller/graphicsController.js
@@ -19,22 +19,33 @@ exports.Graphics = async (req, res) => {
       }
     });
 
-    // graphics.map(el => {
-    //   console.log(el.dataValues);
+    const All = [];
+
+    graphics.map(el => {
+      console.log(
+        All.push({
+          tmp: el.dataValues.tmp,
+          tt: el.dataValues.tt,
+          conso_pred: el.dataValues.conso_pred,
+          conso_measured: el.dataValues.conso_measured,
+          opti: el.dataValues.opti,
+          emulator_serial: el.dataValues.emulator_serial
+        })
+      );
+    });
     // });
-    // // });
-    // // console.log("GRAPHICS", graphics);
+    // console.log("ALL", All);
 
-    const response = {
-      description: "Graphics",
-      tt: graphics[0].tt,
-      conso_pred: graphics[0].conso_pred,
-      conso_measured: graphics[0].conso_measured,
-      opti: graphics[0].opti,
-      emulator_serial: graphics[0].emulator_serial
-    };
+    // const response = {
+    //   description: "Graphics",
+    //   tt: graphics[0].tt,
+    //   conso_pred: graphics[0].conso_pred,
+    //   conso_measured: graphics[0].conso_measured,
+    //   opti: graphics[0].opti,
+    //   emulator_serial: graphics[0].emulator_serial
+    // };
 
-    res.status(200).send(response);
+    res.status(200).send(All);
   } catch (error) {
     console.log(error);
     res.status(500).json({
