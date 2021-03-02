e8f0b72407f49995b4ac7cc83838a95ee204b846
Ines <inz@e-nno.ch>
2021-03-02 15:19:02 +0100

    [ADD] response if lastseen is empty

--- app/controller/graphicsController.js
+++ app/controller/graphicsController.js
@@ -7,6 +7,7 @@ const Op = db.Sequelize.Op;
 exports.Graphics = async (req, res) => {
   const { key } = req.params;
   const lastSeen = req.body.lastSeen;
+  const All = [];
 
   try {
     // GET GRAPHICS DATA
@@ -16,27 +17,40 @@ exports.Graphics = async (req, res) => {
 
     console.log("EMULATOR SERIAL", building_emulator);
 
-    const graphics = await dynamic_graphics.findAll({
-      where: {
-        emulator_serial: building_emulator[0][0].emulator_serial,
-        tmp: { [Op.gt]: lastSeen }
-      }
-    });
-
-    const All = [];
-
-    graphics.map(el => {
-      console.log(
-        All.push({
-          tmp: el.dataValues.tmp,
-          tt: el.dataValues.tt,
-          conso_pred: el.dataValues.conso_pred,
-          conso_measured: el.dataValues.conso_measured,
-          opti: el.dataValues.opti,
-          emulator_serial: el.dataValues.emulator_serial
-        })
-      );
-    });
+    if (!lastSeen || lastSeen === "") {
+      const graphics = await dynamic_graphics.findAll({
+        where: {
+          emulator_serial: building_emulator[0][0].emulator_serial
+        }
+      });
+      All.push({
+        tmp: graphics[0].dataValues.tmp,
+        tt: graphics[0].dataValues.tt,
+        conso_pred: graphics[0].dataValues.conso_pred,
+        conso_measured: graphics[0].dataValues.conso_measured,
+        opti: graphics[0].dataValues.opti,
+        emulator_serial: graphics[0].dataValues.emulator_serial
+      });
+    } else {
+      const graphics = await dynamic_graphics.findAll({
+        where: {
+          emulator_serial: building_emulator[0][0].emulator_serial,
+          tmp: { [Op.gt]: lastSeen }
+        }
+      });
+      graphics.map(el => {
+        console.log(
+          All.push({
+            tmp: el.dataValues.tmp,
+            tt: el.dataValues.tt,
+            conso_pred: el.dataValues.conso_pred,
+            conso_measured: el.dataValues.conso_measured,
+            opti: el.dataValues.opti,
+            emulator_serial: el.dataValues.emulator_serial
+          })
+        );
+      });
+    }
 
     res.status(200).send(All);
   } catch (error) {
